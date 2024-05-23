bcadd940c8a04fa8873a687507510b8ff534ed4a
yunchae-kim <yunchae.dev@gmail.com>
2024-05-22 22:33:05 -0400

    chore: set up project
    This commit sets up the initial project structure.
    
    - Created a new React project with TypeScript.
    - Installed and configured Tailwind CSS.
    - Installed and configured commitlint and husky.
    - Initialized a new GitHub repository.

---
+++ kanban-ui-project/commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 50],
+    'body-max-line-length': [2, 'always', 72],
+  },
+};
